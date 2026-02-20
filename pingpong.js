e3a320f120f25c17cdf904ead97e869b671037b5
webbombom <isadora.siqueira.barbosa@escola.pr.gov.br>
2026-02-20 07:48:13 -0300

    Initialize pingpong game with basic variables

---
+++ pingpong.js
@@ -0,0 +1,29 @@
+//criar bolinha
+let raio = 7.5;
+let xBolinha = 100;
+let yBolinha = 200;
+let diametro = raio * 2;
+
+//velocidade bolinha
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+
+//minha raquete
+let xRaquete = 5;
+letyRaquete = 150;
+
+//placar oponente
+let xOponente = 585;
+let yOponente = 150;
+
+//placar
+let MeusPontos = 0;
+let pontosOponentes = 0;
+
+function setup (){
+  CreateCanvas(600, 400);
+}
+
+function draw(){
+  
+}
